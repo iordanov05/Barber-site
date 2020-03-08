9dffa33c4ec2c9b9154398af98ddef6b52e1bab2
iordanov05 <iordanov05@mail.ru>
2020-03-08 13:00:28 +0300

    Добавляет логику

---
+++ js/script.js
@@ -0,0 +1,22 @@
+var modalOpenButton = document.querySelector('.slider__btn--modalShow');
+
+var modal = document.querySelector('.modal');
+var modalCloseButton = modal.querySelector('.modal__close-btn');
+
+
+modalOpenButton.addEventListener('click', function () {
+  if (modal.classList.contains('modal--hide')) {
+    modal.classList.remove('modal--hide');
+  }
+});
+modalCloseButton.addEventListener('click', function () {
+  if (!modal.classList.contains('modal--hide')) {
+    modal.classList.add('modal--hide');
+  }
+});
+
+document.addEventListener('keydown', function (evt) {
+  if (evt.key === "Escape" && !modal.classList.contains('modal--hide')) {
+    modal.classList.add('modal--hide');
+  }
+});
