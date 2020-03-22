38f97fa302b5582071cc829d4990455a80d76d5a
papatomatoe <hole_in_the_wall@list.ru>
2020-03-22 12:57:42 +0300

    Верстка остальных разделов

--- js/script.js
+++ js/script.js
@@ -5,15 +5,15 @@ var modalCloseButton = modal.querySelector('.modal__close-btn');
 var onModalOpenButtonClick = function () {
   if (modal.classList.contains('modal--hide')) {
     modal.classList.remove('modal--hide');
-    modalCloseButton.addEventListener('click', onModalCloseButton);
+    modalCloseButton.addEventListener('click', onModalCloseButtonClick);
     document.addEventListener('keydown', onDocumentKeydown);
   }
 };
 
-var onModalCloseButton = function () {
+var onModalCloseButtonClick = function () {
   if (!modal.classList.contains('modal--hide')) {
     modal.classList.add('modal--hide');
-    modalCloseButton.removeEventListener('click', onModalCloseButton);
+    modalCloseButton.removeEventListener('click', onModalCloseButtonClick);
     document.removeEventListener('keydown', onDocumentKeydown);
   }
 };
@@ -21,7 +21,7 @@ var onModalCloseButton = function () {
 var onDocumentKeydown = function (evt) {
   if (evt.key === 'Escape' && !modal.classList.contains('modal--hide')) {
     modal.classList.add('modal--hide');
-    modalCloseButton.removeEventListener('click', onModalCloseButton);
+    modalCloseButton.removeEventListener('click', onModalCloseButtonClick);
     document.removeEventListener('keydown', onDocumentKeydown);
   }
 };
